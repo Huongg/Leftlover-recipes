cf877c9beead70a10c2fc182d1584e079b3426c3
Huongg <32060364+Huongg@users.noreply.github.com>
2017-11-01 23:27:36 +0000

    Added Email response button

--- www/script.js
+++ www/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	
 	$('#button').click(function(){
 		var submitEmail = $('input[name=email]').val();
-		var emailResponse = "Thanks for subscribing your email!" + submitEmail ;
-		$('.response').append(emailResponse) ;
-
+		var emailResponse = "Thanks for subscribing your email " + submitEmail + "!";
+		//$('.response').append(emailResponse) ;
+		$('.response').text(emailResponse) ;
 	});
 });
