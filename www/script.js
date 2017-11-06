3a1de6ecfeeb4e4211c1b21b947a0d28c7b90cdc
Huongg <32060364+Huongg@users.noreply.github.com>
2017-11-06 19:04:54 +0000

    Added jQuery toggle code

--- www/script.js
+++ www/script.js
@@ -1,14 +1,23 @@
 
 $(document).ready(function(){
 	
+	$('#checkbox2').click(function() {              
+		$('#recipe-2').toggle();
+        $('#recipe-3').toggle();
+        $('#recipe-4').toggle();
+	});
+
+	$('#checkbox5').click(function() {              
+		$('#recipe-5').toggle();
+        $('#recipe-6').toggle();
+	});
+
+
 	$('#button').click(function(){
 		var submitEmail = $('input[name=email]').val();
-		var emailResponse = "Thanks for subscribing your email " + submitEmail + "!";
+		var emailResponse = "Thanks for subscribing your email " + "!";
 		//$('.response').append(emailResponse) ;
 		$('.response').text(emailResponse) ;
 	});
 
-	//$('.custom-checkbox').change( fuction(){
-		//alert('1');
-	//});	
 });
