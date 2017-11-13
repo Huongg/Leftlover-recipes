42b9431a5c68d84df89b6bb305c14ee9a13c2322
Huongg <32060364+Huongg@users.noreply.github.com>
2017-11-13 15:45:03 +0000

    Added toggle jQuery to custom checkbox

--- www/script.js
+++ www/script.js
@@ -1,17 +1,55 @@
 
 $(document).ready(function(){
 	
-	$('#checkbox2').click(function() {              
-		$('#recipe-2').toggle();
+	$('#checkbox1').click(function() {              
+		$('#recipe-1').toggle();
+        $('#recipe-2').toggle();
         $('#recipe-3').toggle();
         $('#recipe-4').toggle();
+        $('#recipe-5').toggle();
+        $('#recipe-6').toggle();
+        $('#recipe-8').toggle();
+
+	$('#checkbox2').click(function() {              
+		$('#recipe-1').toggle();
+        $('#recipe-3').toggle();
+        $('#recipe-5').toggle();
+        $('#recipe-7').toggle();
+	});
+
+	$('#checkbox3').click(function() {              
+		$('#recipe-2').toggle();
+        $('#recipe-4').toggle();
+        $('#recipe-6').toggle();
+        $('#recipe-8').toggle();
+        $('#recipe-9').toggle();
+	});
+
+	$('#checkbox4').click(function() {              
+		$('#recipe-1').toggle();
+        $('#recipe-3').toggle();
+        $('#recipe-4').toggle();
+        $('#recipe-7').toggle();
+        $('#recipe-8').toggle();
+        $('#recipe-9').toggle();
 	});
 
 	$('#checkbox5').click(function() {              
-		$('#recipe-5').toggle();
+		$('#recipe-2').toggle();
+        $('#recipe-5').toggle();
         $('#recipe-6').toggle();
+        $('#recipe-7').toggle();
+        $('#recipe-8').toggle();
 	});
-
+	$('#checkbox6').click(function() {              
+		$('#recipe-1').toggle();
+        $('#recipe-2').toggle();
+        $('#recipe-3').toggle();
+        $('#recipe-4').toggle();
+        $('#recipe-5').toggle();
+        $('#recipe-6').toggle();
+        $('#recipe-9').toggle();
+	});
 
 	$('#submit-button').click(function(){
 		var submitEmail = $('input[name=email]').val();
